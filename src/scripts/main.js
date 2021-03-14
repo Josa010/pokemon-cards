0d0f395d8562f06e26f1cb38c41e8568a635f3e2
Josa <novairesh@gmail.com>
2021-03-14 16:56:42 -0600

    added 'for' for pokemon types iterations and fixed some css bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@
 const API = 'https://pokeapi.co/api/v2/pokemon?limit=151'
 const POKEMONS_TOTALES = 150
 const cardsContainer = document.querySelector('.cards-container')
+const typeNames = ['normal','fire','water','grass','electric','ice','fighting','poison','ground','flying','psychic','bug','rock','ghost','dark','dragon','steel','fairy']
 
 //hacemos peticiones fetch
 fetch(API)
@@ -81,140 +82,21 @@ const crearCards = (nombrePokemon, url) => {
     card.append(divTypes)
 
     //Por ulitmo, hacemos un for para iterar todos los tipos del pokemon
+
+    
+
     for (let i = 0; i < types; i++) {
-        
         let type = url.types[i].type.name
         let divType
         
-        //Hacemos un switch donde creamos el div, le ponemos su clase y lo
-        //Metemos dentro del divTypes, despues dependiendo al tipo, le ponemos
-        //Una clase especial y le ponemos el nombre del tipo
-        switch(type){
-            case 'normal': 
+        for (let i = 0; i < typeNames.length; i++) {
+            if (type === typeNames[i]) {
                 divType = document.createElement('div')
-                divType.classList.add('normal')
-                divType.innerHTML = 'Normal'
+                divType.classList.add(typeNames[i])
+                divType.innerHTML = `${typeNames[i].charAt(0).toUpperCase()}${typeNames[i].slice(1)}`
                 divTypes.append(divType)
-                break;
-
-            case 'fire': 
-                divType = document.createElement('div')
-                divType.classList.add('fuego')
-                divType.innerHTML = 'Fuego'
-                divTypes.append(divType)
-                break;
-
-            case 'water': 
-                divType = document.createElement('div')
-                divType.classList.add('agua')
-                divType.innerHTML = 'Agua'
-                divTypes.append(divType)
-                break;
-                
-            case 'grass': 
-                divType = document.createElement('div')
-                divType.classList.add('planta')
-                divType.innerHTML = 'Planta'
-                divTypes.append(divType)
-                break;
-
-            case 'electric': 
-                divType = document.createElement('div')
-                divType.classList.add('electrico')
-                divType.innerHTML = 'Electrico'
-                divTypes.append(divType)
-                break;
-
-            case 'ice': 
-                divType = document.createElement('div')
-                divType.classList.add('hielo')
-                divType.innerHTML = 'Hielo'
-                divTypes.append(divType)
-                break;
-
-            case 'fighting': 
-                divType = document.createElement('div')
-                divType.classList.add('lucha')
-                divType.innerHTML = 'Lucha'
-                divTypes.append(divType)
-                break;
-
-            case 'poison': 
-                divType = document.createElement('div')
-                divType.classList.add('veneno')
-                divType.innerHTML = 'Veneno'
-                divTypes.append(divType)
-                break;
-
-            case 'ground': 
-                divType = document.createElement('div')
-                divType.classList.add('tierra')
-                divType.innerHTML = 'Tierra'
-                divTypes.append(divType)
-                break;
-                
-            case 'flying': 
-                divType = document.createElement('div')
-                divType.classList.add('volador')
-                divType.innerHTML = 'Volador'
-                divTypes.append(divType)
-                break;
-
-            case 'psychic': 
-                divType = document.createElement('div')
-                divType.classList.add('psiquico')
-                divType.innerHTML = 'Psiquico'
-                divTypes.append(divType)
-                break;
-
-            case 'bug': 
-                divType = document.createElement('div')
-                divType.classList.add('bicho')
-                divType.innerHTML = 'Bicho'
-                divTypes.append(divType)
-                break;
-
-            case 'rock': 
-                divType = document.createElement('div')
-                divType.classList.add('roca')
-                divType.innerHTML = 'Roca'
-                divTypes.append(divType)
-                break;
+            }
             
-            case 'ghost': 
-                divType = document.createElement('div')
-                divType.classList.add('fantasma')
-                divType.innerHTML = 'Fantasma'
-                divTypes.append(divType)
-                break;
-                
-            case 'dark': 
-                divType = document.createElement('div')
-                divType.classList.add('oscuro')
-                divType.innerHTML = 'Oscuro'
-                divTypes.append(divType)
-                break;
-
-            case 'dragon': 
-                divType = document.createElement('div')
-                divType.classList.add('dragon')
-                divType.innerHTML = 'Dragon'
-                divTypes.append(divType)
-                break;
-
-            case 'steel': 
-                divType = document.createElement('div')
-                divType.classList.add('acero')
-                divType.innerHTML = 'Acero'
-                divTypes.append(divType)
-                break;
-
-            case 'fairy': 
-                divType = document.createElement('div')
-                divType.classList.add('hada')
-                divType.innerHTML = 'Hada'
-                divTypes.append(divType)
-                break;
         }
         //Por ultimo, a todos los divs le ponemos la clase type
         divType.classList.add('type')
